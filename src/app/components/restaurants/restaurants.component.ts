6b67f6614f1d508c47107c9fa7d889ef21b67978
jhon cardenas <jhoncardenas@iMac-de-jhon.local>
2019-12-11 17:29:28 -0500

    push img

--- src/app/components/restaurants/restaurants.component.ts
+++ src/app/components/restaurants/restaurants.component.ts
@@ -9,10 +9,23 @@ import { ServiceService } from '../../services/service.service';
 export class RestaurantsComponent implements OnInit {
 
   data: any;
+  imgs = [{id: 1, url: '../../../assets/img/stroller_pizza.png'}, {id: 2, url: '../../../assets/img/sbarro_pizza.png'}];
 
   constructor(private services: ServiceService) { }
 
   async ngOnInit() {
     this.data = await this.services.getData();
+  }
+
+  getImg(restaurant: any) {
+    // console.log(restaurant);
+    let temp: string;
+    this.imgs.forEach(element => {
+      if (element.id === restaurant.id) {
+        console.log(element.url);
+        temp = element.url;
+      }
+    });
+    return temp;
   }
 }
