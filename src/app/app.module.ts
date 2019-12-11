198fadbccb8738ae6e279d1c92ff1a6927a9c092
jhoncardenas11 <jhoncardenas.11@gmail.com>
2019-12-10 21:53:34 -0500

    proyect & components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RestaurantsComponent } from './components/restaurants/restaurants.component';
+import { RestaurantDetailComponent } from './components/restaurant-detail/restaurant-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RestaurantsComponent,
+    RestaurantDetailComponent
   ],
   imports: [
     BrowserModule,
