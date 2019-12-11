58c5725befaa2b1519e672ebbe1f5a77787d7d0e
jhoncardenas11 <jhoncardenas.11@gmail.com>
2019-12-10 23:39:25 -0500

    first part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RestaurantsComponent } from './components/restaurants/restaurants.component';
+import { RestaurantDetailComponent } from './components/restaurant-detail/restaurant-detail.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'singin', component: LoginComponent},
+  {path: 'restaurants', component: RestaurantsComponent},
+  {path: 'restaurant/:id', component: RestaurantDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
