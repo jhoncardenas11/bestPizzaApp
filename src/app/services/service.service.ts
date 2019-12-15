cc04be10c5fd9e36047981a1e89d728711ccb70c
jhoncardenas11 <jhoncardenas.11@gmail.com>
2019-12-15 09:24:08 -0500

    last changes

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class ServiceService {
   constructor(private http: HttpClient) { }
 
   getData() {
-    return this.http.get(`https://raw.githubusercontent.com/kevmuy/test-frontend/master/RH.json`).toPromise();
+    return this.http.get(`${environment.url}/kevmuy/test-frontend/master/RH.json`).toPromise();
   }
 }
